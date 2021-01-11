fd7b578f1a0246f8a10200e71244a8f7e2162856
Gladys Jimenez Alvarez <v-gladysj@microsoft.com>
2021-01-10 21:26:50 -0600

    test driven development

---
+++ Calculator/SalaryCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class SalaryCalculator
+    {
+        const int HoursInYear = 2080;
+        public decimal GetAnnualSalary(decimal hourlyWage)
+        {
+            
+            decimal annualSalary = hourlyWage * HoursInYear;
+            return annualSalary;
+        }
+        // Example using lambda =>
+        public decimal GetHourlyWage(int annualSalary) => annualSalary / HoursInYear;
+        
+    }
+}
